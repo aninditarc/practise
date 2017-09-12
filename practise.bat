e1e8f3d4d5308563bd6c26eace5ec17de5900993
aninditarc <arc10031995@gmail.com>
2017-09-12 18:10:16 +0530

    first commit

--- practise.bat
+++ practise.bat
@@ -1,2 +1,38 @@
 @echo off
-echo.>"C:\Users\a.c.roychowdhury\Documents\practise\practise1.txt"
+:start
+echo -create(to create password)
+echo -check(to reconfirm the password)
+echo.
+pause
+set /p program=do you want to create or check password ?:
+goto %program%
+            
+
+
+
+:create
+set /p Password=What password do you want to set??
+echo %Password%>C:\Users\a.c.roychowdhury\Documents\practise\practise1.txt
+pause
+goto check
+
+
+
+
+:check
+set /p RPassword=Reenter your password:
+for /f "Delims=" %%a in (practise1.txt) do (
+set TEXT=%%a
+)
+if %RPassword%==%TEXT% goto correct 
+echo you are wrong,Try again
+pause 
+goto check
+
+
+:correct
+echo your password is correct
+
+
+
+ 
